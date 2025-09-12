a7fd2170a49eebdcf1b580fb46de77b8dc98e378
Diego <diego.aparicio@etu.unistra.fr>
2025-09-12 11:56:10 +0200

    Ajout de la map

--- src/Main/main.gd
+++ src/Main/main.gd
@@ -7,10 +7,11 @@ extends Node2D
 @onready var stage: Label = $MainScene/Stage
 
 
-func _on_button_pressed() -> void:
-	for auditor: Auditor in auditors.get_children():
-		auditor.create_multiple_audits()
-
-
 func _on_confidence_bar_value_changed(value: float) -> void:
 	stage.text = "Stage %d (%d/%d)" % [confidence_bar.stage, confidence_bar.value, confidence_bar.max_value]
+
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("create_audits"):
+		for auditor: Auditor in auditors.get_children():
+			auditor.create_multiple_audits()
