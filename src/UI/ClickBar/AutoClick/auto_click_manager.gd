c0350062d731525310f1a7b607a118194208c11b
Diego <diego.aparicio@etu.unistra.fr>
2025-12-17 10:39:15 +0100

    Début d'ajout de commentaires du code

--- src/UI/ClickBar/AutoClick/auto_click_manager.gd
+++ src/UI/ClickBar/AutoClick/auto_click_manager.gd
@@ -1,15 +1,19 @@
 extends Node
 class_name AutoClickManager
 
+
 var clickbars: Array[ClickBarDisplay] = []
 
 
 func _ready() -> void:
+	# Connecter le signal
 	SignalBus.update_auto_clicks.connect(_on_update_auto_clicks)
 
 
 func _physics_process(delta: float) -> void:
+	# Augmenter progressivement les ClickBarDisplay
 	for click_bar_display: ClickBarDisplay in clickbars:
+		# Erreur si les clics par seconde sont inférieurs à 0
 		if click_bar_display.cps < 0:
 			push_error("%s : ClickBarDisplay %s isn't registered in the dictionnary" % [name, click_bar_display.name])
 			clickbars.erase(click_bar_display)
@@ -17,12 +21,19 @@ func _physics_process(delta: float) -> void:
 			click_bar_display.click_bar.add_value(click_bar_display.cps * delta)
 
 
+## Procédure qui ajoute un nouveau ClickBarDisplay passé en paramètre à la liste des clickbars.
+## @params click_bar_display Le nouveau ClickBarDisplay.
 func add_new_click_bar_display(click_bar_display: ClickBarDisplay) -> void:
+	# Ajouter un nouveau ClickBarDisplay
 	clickbars.append(click_bar_display)
+	# Connecter le signal
 	click_bar_display.distribution_bar.value_updated.connect(_on_distribution_bar_value_updated)
 
 
+## Procédure handler du signal de mise à jour des clics passifs.
+## @params value La valeur des clicks passifs.
 func _on_update_auto_clicks(value: float) -> void:
+	# Mettre à jour la distribution
 	if get_distribution_percentage_remainder(-1) == 100.0:
 		clickbars[0].distribution_bar.value = 100.0
 		_on_distribution_bar_value_updated(clickbars[0].distribution_bar)
@@ -30,14 +41,21 @@ func _on_update_auto_clicks(value: float) -> void:
 		update_all_cps()
 
 
+## Procédure handler du signal de changement de la distribution des clics passifs.
+## @params bar La barre de distribution.
 func _on_distribution_bar_value_updated(bar: DistributionBar) -> void:
+	# Mettre à jour la distribution
 	var max_value: float = get_distribution_percentage_remainder(bar.get_instance_id())
 	bar.value = clampf(bar.value, 0.0, max_value)
 	update_cps(bar.get_instance_id(), bar.value)
 
 
+## Fonction qui renvoie un flottant correspondant au pourcentage restant de répartition des clics passifs.
+## @params bar_id L'identifiant d'instance de la barre de distribution.
+## @return float Le pourcentage de répartition restant.
 func get_distribution_percentage_remainder(bar_id: int) -> float:
 	var total_remainder: float = 100
+	# Calculer le reste
 	for click_bar_display: ClickBarDisplay in clickbars:
 		var distribution_bar: DistributionBar = click_bar_display.distribution_bar
 		if distribution_bar.get_instance_id() != bar_id:
@@ -45,10 +63,14 @@ func get_distribution_percentage_remainder(bar_id: int) -> float:
 	return total_remainder
 
 
+## Procédure qui met à jour les clics par seconde d'un certain ClickBarDisplay.
+## @params bar_id L'identifiant d'instance de la barre de distribution du ClickBarDisplay recherché.
+##         value La valeur des clics par seconde du ClickBarDiplay recherché.
 func update_cps(bar_id: int, value: float) -> void:
 	var n: int = clickbars.size()
 	var i: int = 0
 	var found: bool = false
+	# Retrouver le ClickBarDisplay
 	while i<n && !found:
 		var click_bar_display: ClickBarDisplay = clickbars[i]
 		var distribution_bar: DistributionBar = click_bar_display.distribution_bar
@@ -61,6 +83,7 @@ func update_cps(bar_id: int, value: float) -> void:
 		push_error("%s : The ClickBarDisplay wasn't found" % name)
 
 
+## Procédure qui met à jour l'affichage des clics par seconde de chaque ClickBarDisplay.
 func update_all_cps() -> void:
 	for click_bar_display: ClickBarDisplay in clickbars:
 		var value: float = click_bar_display.distribution_bar.value
--- src/UI/ClickBar/click_bar.gd
+++ src/UI/ClickBar/click_bar.gd
@@ -12,45 +12,61 @@ signal cycle_completed(id: int, cycles: int)
 
 
 func _process(_delta: float) -> void:
+	# Affichage dans l'éditeur
 	if Engine.is_editor_hint():
 		update_color()
 
 
 func _ready() -> void:
+	# Mise à jour au lancement du jeu
 	if Engine.is_editor_hint():
 		return
 	set_process(false)
 	update_color()
 
 
+## Procédure qui met à jour la couleur de la barre de progression.
 func update_color() -> void:
-	if progress_bar:
-		var style_box = StyleBoxFlat.new()
-		style_box.set_corner_radius_all(5)
-		style_box.bg_color = color
-		progress_bar.add_theme_stylebox_override("fill", style_box)
+	if !progress_bar: return
+	# Mise à jour
+	var style_box = StyleBoxFlat.new()
+	style_box.set_corner_radius_all(5)
+	style_box.bg_color = color
+	progress_bar.add_theme_stylebox_override("fill", style_box)
 
 
+## Procédure qui ajoute de la valeur en fonction du nombre de clicks.
+## @params nb_clicks Le nombre de clics.
 func add_value(nb_clicks: float) -> void:
 	if nb_clicks > 0:
+		# Valeur ajoutée = valeur max * (clics / clics requis) + sécurité
 		progress_bar.add_value(progress_bar.max_value * (nb_clicks / required_clicks) + 0.01)
 
 
+## Procédure qui met à jour la valeur de la barre de progression.
+## @params value La nouvelle valeur.
 func set_value(value: float) -> void:
 	progress_bar.value = value
 
 
+## Fonction qui renvoie la valeur de la barre de progression.
+## @return float La valeur.
 func get_value() -> float:
 	return progress_bar.value
 
 
+## Procédure handler du clic.
 func _on_pressed() -> void:
+	# Ajouter la valeur correspondant aux clics
 	add_value(Global.get_clicks())
 
 
+## Procédure handler du signal du maximum atteint.
 func _on_progress_bar_maximum_reached(cycles: int) -> void:
+	# Emettre le signal
 	cycle_completed.emit(get_instance_id(), cycles)
 
-
+## Procédure qui réinitialise la barre de progression.
+## @params bool Emettre un signal lors de la réinitialisation si true.
 func reset(no_signal: bool) -> void:
 	progress_bar.reset(no_signal)
