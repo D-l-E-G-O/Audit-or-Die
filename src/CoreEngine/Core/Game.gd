476df2e05db1a2825ad95d9d34f88e4252e7c767
Diego <diego.aparicio@etu.unistra.fr>
2025-12-17 17:27:52 +0100

    Curseurs personnalisés

--- src/CoreEngine/Core/Game.gd
+++ src/CoreEngine/Core/Game.gd
@@ -8,6 +8,11 @@ var current_scene: Node2D = null
 
 
 func _ready() -> void:
+	# Définir les images de curseur de la souris
+	var cursor_default = preload("res://assets/cursors/hand_cursor.png")
+	var cursor_hover = preload("res://assets/cursors/hand_hover_cursor.png")
+	Input.set_custom_mouse_cursor(cursor_default, Input.CURSOR_ARROW, Vector2(12, 0))
+	Input.set_custom_mouse_cursor(cursor_hover, Input.CURSOR_POINTING_HAND, Vector2(12, 7))
 	# Récupérer les noeuds
 	App = get_tree().root.get_node("App")
 	Scene = App.get_node("SceneRoot")
